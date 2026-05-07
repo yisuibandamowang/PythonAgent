af29186dc0e8cc395584de62ee11b0cd740e58be
我只有一岁半 <2307585872@qq.com>
2026-05-07 22:20:03 +0800

    refactor(structure): 重构目录结构并添加Chain基础使用示例

---
+++ P3langchain使用/18.python或运算符的重写.py
@@ -0,0 +1,26 @@
+class Test(object):
+    def __init__(self,name):
+        self.name = name
+    def __or__(self, other):
+        return MySequence(self,other)
+    def __str__(self):
+        return self.name
+
+class MySequence(object):
+    def __init__(self,*args):
+        self.sequence = []
+        for arg in args:
+            self.sequence.append(arg)
+    def __or__(self, other):
+        self.sequence.append(other)
+        return self
+    def run (self):
+        for item in self.sequence:
+            print(item)
+
+if __name__ == '__main__':
+    a = Test("a")
+    b = Test("b")
+    c = Test("c")
+    d = a | b | c
+    d.run()
