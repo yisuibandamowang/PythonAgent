cbbe4845f439c3d7974df986be6485b930187cb7
chenhongyi <chenhongyi@360.cn>
2026-06-26 18:07:20 +0800

    feat(agent): 添加AutoGen团队协作案例及更新ReAct智能体查询问题

--- p7_hello_agent/chapter4/ReAct.py
+++ p7_hello_agent/chapter4/ReAct.py
@@ -102,5 +102,5 @@ if __name__ == '__main__':
     search_desc = "一个网页搜索引擎。当你需要回答关于时事、事实以及在你的知识库中找不到的信息时，应使用此工具。"
     tool_executor.registerTool("Search", search_desc, search)
     agent = ReActAgent(llm_client=llm, tool_executor=tool_executor)
-    question = "华为最新的手机是哪一款？它的主要卖点是什么？"
+    question = "华为最新（截止现在2026）的手机是哪一款？它的主要卖点是什么？"
     agent.run(question)
